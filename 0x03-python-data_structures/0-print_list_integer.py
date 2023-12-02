977bf34ec2ec293b1036ce2753ee87def9753203
Waiganjo <waiganjothiongo12@gmail.com>
2023-12-01 22:23:35 -0800

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(i))
+def print_list_integer(a=[]):
+    for i in a:
+        print('{:d}'.format(i))
 
