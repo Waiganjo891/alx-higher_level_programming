aac57ff3bf614315f490e8d9706aae662fe3268e
Waiganjo <waiganothiongo12@gmail.com>
2024-01-08 02:29:48 -0800

    Task 2

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Inherits a class-checking function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if an object is an instance or inherited instance of a class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
