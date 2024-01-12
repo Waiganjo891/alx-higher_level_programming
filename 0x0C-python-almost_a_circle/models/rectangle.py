cd5ccfcc3283cd14f21b5ac1e9b4137e13eafafc
Elijah <waiganjothiongo12@gmail.com>
2024-01-12 00:41:11 -0800

    Task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,6 +65,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
     def area(self):
         """Return the area of the Rectangle."""
         return self.width * self.height
@@ -74,9 +75,14 @@ class Rectangle(Base):
         if self.width == 0 or self.height == 0:
             print("")
             return
-
         [print("") for y in range(self.y)]
         for h in range(self.height):
             [print(" ", end="") for x in range(self.x)]
             [print("#", end="") for w in range(self.width)]
             print("")
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
