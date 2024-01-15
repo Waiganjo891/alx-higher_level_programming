fd8dc13b87d3d06a4726b52c2560d965e1331a69
Your Name <you@example.com>
2024-01-15 04:19:42 -0800

    Task 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,11 +6,18 @@ import turtle
 
 
 class Base:
-   __nb_objects = 0
+    __nb_objects = 0
 
-   def __init__(self, id=None):
-       if id is not None:
-           self.id = id
-       else:
-           Base.__nb_objects += 1
-           self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
