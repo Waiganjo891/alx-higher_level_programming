19297f0abbd32311cfbacbb5b3596e06385a1e4f
Your Name <you@example.com>
2024-03-28 05:45:29 -0700

    Task 2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Check if URL argument is provided
-# Send DELETE request using curl and display the body of the response
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-curl -X DELETE -s "$1"
+# Sends a DELETE request to the specified URL and displays the body of the response
+curl -s -X DELETE "$1"
