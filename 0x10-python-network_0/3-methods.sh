fb5cc94b1a442834d246f7a2f0c7d5c95a81ea61
Your Name <you@example.com>
2024-03-28 05:50:36 -0700

    Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Check if URL argument is provided
-# Send OPTIONS request using curl to fetch allowed HTTP methods
-# Display the allowed HTTP methods
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-allowed_methods=$(curl -s -I -X OPTIONS "$1" | awk '/Allow/{print $2}')
-echo "$allowed_methods"
+# Takes in a URL and displays all HTTP methods the server will accept
+curl -s -i -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f2-
