38ac39362aab4db6c7af914aa6f1eaba6b1f0dad
Your Name <you@example.com>
2024-03-28 06:18:57 -0700

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script fetches the status from,
+https://alx-intranet.hbtn.io/status using the urllib package.
+"""
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body.decode('utf-8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+# Repository Description
+
+This repository contains a Python script that fetches the status from https://alx-intranet.hbtn.io/status using the urllib package.
