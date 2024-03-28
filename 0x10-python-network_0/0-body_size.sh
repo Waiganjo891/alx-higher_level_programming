9ff1fa27af73d5a15359142c1cc36ec9484fa2d4
Your Name <you@example.com>
2024-03-28 05:25:39 -0700

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Check if a URL is provided
-# Send request using curl and get the size of the response body in bytes
-# Display the size of the response body
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-response_size=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-echo "$response_size"
+# This script sends a request to a given URL and displays the size of the response body in bytes.
+curl -s -w "\n%{size_download}" "$1"
