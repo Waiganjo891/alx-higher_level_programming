728af209c7c3a8c5f6989ed276392a929a270293
Your Name <you@example.com>
2024-04-23 01:23:35 -0700

    Task 4

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,8 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-
   if (error) {
     console.error('Error reading the file:', error);
-
   } else {
     console.log(content);
   }
